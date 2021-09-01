bbb5606264afb97ba831a6edf41c89882efb9128
An Do Van (Intern-Tech Pools-Technology-VND HO) <an.do@vndirect.com.vn>
2021-09-01 16:45:04 +0700

    Ad

--- src/app/models/productBoy.js
+++ src/app/models/productBoy.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productBoy = new Schema();
-module.exports = mongoose.model("Productboys", productBoy);
+const ProductBoy = new Schema();
+module.exports = mongoose.model("Productboys", ProductBoy);
