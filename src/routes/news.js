13c37246eb6d27a5f9a719d143270991b94b5902
An Do Van (Intern-Tech Pools-Technology-VND HO) <an.do@vndirect.com.vn>
2021-09-02 15:29:36 +0700

    a

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ProductInfos = require("../app/controllers/productInfo");
+const ProductInfos = require("../app/controllers/controller.info.js");
 
 const news = express.Router();
 
