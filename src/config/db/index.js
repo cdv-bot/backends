2756392b8a45ab173dd025dcecedc16e64e313a8
An Do Van (Intern-Tech Pools-Technology-VND HO) <an.do@vndirect.com.vn>
2021-09-04 22:28:53 +0700

    a

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/product_dev", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://doan:Ancoi123@cluster0.6zgiz.mongodb.net/product",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      }
+    );
   } catch (error) {
     console.log(error);
   }
